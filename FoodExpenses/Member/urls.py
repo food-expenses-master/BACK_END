b0288eb0cc03387bc9c8ab99d024a579d555c923
yl0937 <yl1033371@gmail.com>
2025-09-29 23:04:45 +0900

    fix : 명시적 import

--- FoodExpenses/Member/urls.py
+++ FoodExpenses/Member/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import *
+from .views import UserRegisterAPIView, UserAuthAPIView, TokenRefreshAPIView
 
 urlpatterns = [
     path("", UserRegisterAPIView.as_view()),
