fc4feec380aed18124fcb5569a8fff3731f090f5
yl0937 <yl1033371@gmail.com>
2025-10-10 22:33:14 +0900

    fix : food plp allow any

--- food/views.py
+++ food/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.exceptions import ValidationError
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from food.serializers import FoodListSerializer
@@ -14,6 +15,7 @@ from food.models import Food
 class FoodListGenericAPIView(GenericAPIView, ListModelMixin):
     queryset = Food.objects.all()
     serializer_class = FoodListSerializer
+    permission_classes = (AllowAny,)
 
     def get_queryset(self):
         qs = super().get_queryset()
