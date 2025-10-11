1783e5de73b304b7c6e5dcc541186e25d34e0f26
yl0937 <yl1033371@gmail.com>
2025-10-12 03:54:37 +0900

    fix : add CORS ALLOWED HOSTS

--- FoodExpenses/settings.py
+++ FoodExpenses/settings.py
@@ -153,7 +153,11 @@ APPEND_SLASH = False
 
 CORS_ALLOWED_ORIGINS = [
     "https://tem-front-end.vercel.app",
-    "http://localhost:3000", ]
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1",
+    "http://127.0.0.1:3000"
+]
 
 CORS_ALLOW_CREDENTIALS = True
 SESSION_COOKIE_SAMESITE = "None"
